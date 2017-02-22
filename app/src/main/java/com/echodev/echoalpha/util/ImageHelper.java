8f0c5ca6b9e882d87dfc29a05a02d38a64d304c9
Kakit Ho <hkk565@gmail.com>
2017-02-23 01:42:01 +0800

    Remove unused code

--- app/src/main/java/com/echodev/echoalpha/util/ImageHelper.java
+++ app/src/main/java/com/echodev/echoalpha/util/ImageHelper.java
@@ -73,38 +73,6 @@ public class ImageHelper {
         imgView.setImageBitmap(bitmap);
     }
 
-//    public static void setPicFromResources(ImageView imgView, Resources appRes, int imgID, TextView debugText) {
-//        // Get the dimensions of the bitmap
-//        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-//        bmOptions.inJustDecodeBounds = true;
-//        BitmapFactory.decodeResource(appRes, imgID, bmOptions);
-//        int photoW = bmOptions.outWidth;
-//        int photoH = bmOptions.outHeight;
-//        float photoScale = (float) photoW/photoH;
-//
-//        // Get the dimensions of the View
-//        int targetW = imgView.getWidth();
-//        int targetH = Math.round(targetW/photoScale);
-//
-//        // Determine how much to scale down the image
-//        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-//
-//        // Decode the image file into a Bitmap sized to fill the View
-//        bmOptions.inJustDecodeBounds = false;
-//        bmOptions.inSampleSize = scaleFactor;
-//        bmOptions.inPurgeable = true;
-//
-//        Bitmap bitmap = BitmapFactory.decodeResource(appRes, imgID, bmOptions);
-//        imgView.setImageBitmap(bitmap);
-//
-//        String debug_msg = "raw photo W: " + photoW + "  H: " + photoH;
-//        debug_msg += "\n" + "raw photo scale: " + photoScale;
-//        debug_msg += "\n" + "target photo W: " + targetW + "  H: " + targetH;
-//        debug_msg += "\n" + "scale factor W: " + photoW/targetW + "  H: " + photoH/targetH;
-//
-//        debugText.setText(debug_msg);
-//    }
-
     public static void setPicFromFile(ImageView imgView, String imgPath) {
         // Get the dimensions of the bitmap
         BitmapFactory.Options bmOptions = new BitmapFactory.Options();
@@ -129,38 +97,6 @@ public class ImageHelper {
         Bitmap bitmap = BitmapFactory.decodeFile(imgPath, bmOptions);
         imgView.setImageBitmap(bitmap);
     }
-
-//    public static void setPicFromFile(ImageView imgView, String imgPath, TextView debugText) {
-//        // Get the dimensions of the bitmap
-//        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-//        bmOptions.inJustDecodeBounds = true;
-//        BitmapFactory.decodeFile(imgPath, bmOptions);
-//        int photoW = bmOptions.outWidth;
-//        int photoH = bmOptions.outHeight;
-//        float photoScale = (float) photoW/photoH;
-//
-//        // Get the dimensions of the View
-//        int targetW = imgView.getWidth();
-//        int targetH = Math.round(targetW/photoScale);
-//
-//        // Determine how much to scale down the image
-//        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-//
-//        // Decode the image file into a Bitmap sized to fill the View
-//        bmOptions.inJustDecodeBounds = false;
-//        bmOptions.inSampleSize = scaleFactor;
-//        bmOptions.inPurgeable = true;
-//
-//        Bitmap bitmap = BitmapFactory.decodeFile(imgPath, bmOptions);
-//        imgView.setImageBitmap(bitmap);
-//
-//        String debug_msg = "raw photo W: " + photoW + "  H: " + photoH;
-//        debug_msg += "\n" + "raw photo scale: " + photoScale;
-//        debug_msg += "\n" + "target photo W: " + targetW + "  H: " + targetH;
-//        debug_msg += "\n" + "scale factor W: " + photoW/targetW + "  H: " + photoH/targetH;
-//
-//        debugText.setText(debug_msg);
-//    }
 
     public static File createImageFile(File storageDir) throws IOException {
         // Create an image file name
