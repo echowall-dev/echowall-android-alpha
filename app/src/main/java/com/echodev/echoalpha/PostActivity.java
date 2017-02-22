ff74de11c3ef79c80daaf54e45dd437525bf714a
Kakit Ho <hkk565@gmail.com>
2017-02-23 01:31:55 +0800

    Remove unused libraries

--- app/src/main/java/com/echodev/echoalpha/PostActivity.java
+++ app/src/main/java/com/echodev/echoalpha/PostActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
--- app/src/main/java/com/echodev/echoalpha/WallActivity.java
+++ app/src/main/java/com/echodev/echoalpha/WallActivity.java
@@ -2,26 +2,16 @@ package com.echodev.echoalpha;
 
 import android.content.Context;
 import android.content.Intent;
-import android.media.MediaPlayer;
-import android.media.MediaRecorder;
-import android.net.Uri;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
 import android.support.design.widget.Snackbar;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.echodev.echoalpha.util.AudioHelper;
-import com.echodev.echoalpha.util.ImageHelper;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -29,8 +19,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.io.File;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -38,7 +26,6 @@ import java.util.UUID;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import butterknife.OnTouch;
 
 public class WallActivity extends AppCompatActivity {
     private static final String LOG_TAG = "Record_audio_message";
