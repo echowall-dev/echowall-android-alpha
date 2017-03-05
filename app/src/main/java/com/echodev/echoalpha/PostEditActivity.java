ef9daea0df55ae48f8e6f0bfab0a36dbbc35c43a
Kakit Ho <hkk565@gmail.com>
2017-03-05 22:29:14 +0800

    Add new Activities for post handling

---
+++ app/src/main/java/com/echodev/echoalpha/PostEditActivity.java
@@ -0,0 +1,13 @@
+package com.echodev.echoalpha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_edit);
+    }
+}
